927e28eb9f6b6dfe13f99d63e419fb5e4f3aa427
vadim-ivlev <vadim.ivlev@gmail.com>
2021-06-07 02:15:00 +0300

    .

--- api/text.go
+++ api/text.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 // TextHandler все о запросе
 func TextHandler(w http.ResponseWriter, r *http.Request) {
+	dir, _ := os.Getwd()
+	fmt.Fprintln(w, "Getwd():", dir)
+
+	files, _ := ioutil.ReadDir(".")
+	for _, file := range files {
+		fmt.Fprintln(w, file.Name())
+	}
+
 	dat, err := ioutil.ReadFile("txt.txt")
 	if err != nil {
 		fmt.Fprint(w, err)
